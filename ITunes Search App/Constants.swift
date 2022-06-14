b5fc2724e2303f1c12a6c71096a7f2d933ce64e0
kgbCat <delova.anna@icloud.com>
2022-06-14 16:33:44 +0300

    create Constants file

---
+++ ITunes Search App/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ITunes Search App
+//
+//  Created by Anna Delova on 6/14/22.
+//
+
+import Foundation
+import UIKit
+
+class Constants {
+    static let loginViewController = "LoginViewController"
+    static let registerViewController = "RegisterViewController"
+    static let searchAlbumViewController = "SearchAlbumViewController"
+    static let albumTracksViewController = "AlbumTracksViewController"
+    static let main = UIStoryboard(name: "Main", bundle: nil)
+}
